b7d892fb9b872e32addc40b9f0f6d166f5ad5077
Dailos Sabina <alu0100353259@ull.edu.es>
2014-10-19 10:13:42 +0100

    Primera version de la clase preguntas de la prct05

--- lib/Preguntas.rb
+++ lib/Preguntas.rb
@@ -1,1 +1,50 @@
 #!/usr/bin/env ruby
+
+  class Pregunta
+    
+    attr_accessor :answers, :title
+
+    
+     def obtener_pregunta
+       puts "Introduzca la pregunta"
+       title = gets.chomp
+       return title
+    end
+
+    def obtener_respuestas
+       puts "Introduzca numero de respuestas a la pregunta"
+       num = gets.chomp.to_i
+       c = []
+       while num != 0 do 
+         c = obtener_respuesta + c
+         num = num - 1 
+       end
+       @answers = c
+   end
+
+    def obtener_respuesta
+      puts "Introduzca la respuesta"
+      ans = [gets.chomp]
+      return ans
+    end
+
+
+    def initialize(title = obtener_pregunta, answers = obtener_respuestas)
+      raise ArgumentError, "Title has to be a String, got #{title.class}" unless title.is_a? String
+      @title = title
+      @answers = answers
+    end
+  
+    def to_s
+      out = "# #{@title}" + "\n"
+      i = 1
+      answers.each do |a|
+        out << " #{i}.- #{a}\n"
+        i += 1
+      end
+      puts out
+    end
+ 
+ end
+
+Pregunta.new().to_s
---
+++ spec/Preguntas_spec.rb
@@ -0,0 +1,31 @@
+
+require_relative '../lib/Preguntas'
+
+  describe Pregunta do
+ 
+    before :each do
+      @p1 = Pregunta.new("Cuanto es 2 mas dos?", ["4","3","1"])  
+    end
+
+    it "Debe existir una pregunta" do
+      expect(@p1.title).not_to be_empty
+    end
+   
+    it "Deben existir opciones de respuestas" do
+      expect(@p1.answers).not_to be_empty
+    end
+
+    it "Se debe invocar a un metodo para obtener la pregunta" do
+       expect(@p1.respond_to? :obtener_pregunta)
+    end
+   
+    it "Se debe invocar a un metodo para obtener las opciones de respuesta" do
+       expect(@p1.respond_to? :obtener_respuestas)
+    end
+    
+    it "Se debe mostrar por la consola la pregunta y las opciones de respuesta" do
+      expect(@p1.respond_to? :to_s)  
+    end
+  end
+
+
